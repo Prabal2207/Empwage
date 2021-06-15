202b06282c93ca2b9f4d610b45588498b2e600ac
Prabal2207 <prabal760@gmail.com>
2021-06-15 19:20:33 +0530

    Attendance check added

--- Employeecomputatio.sh
+++ Employeecomputatio.sh
@@ -1,3 +1,16 @@
 #! /bin/bash -x
 
 echo " welcome to empcomputation"
+is_present=1
+#variable
+p=$(( RANDOM % 2 ))
+
+#selection statement
+if [ $p -eq $is_present ]
+then
+ echo "ABSENT"
+else
+ echo "PRESENT"
+fi
+
+
