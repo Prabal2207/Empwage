f6aa64af7c2793d9a8456f07b0cca88de194c779
Prabal2207 <prabal760@gmail.com>
2021-06-16 13:10:16 +0530

    salary calculation is added

--- Employeecomputatio.sh
+++ Employeecomputatio.sh
@@ -1,16 +1,22 @@
 #! /bin/bash -x
 
 echo " welcome to empcomputation"
+emp_wageperhr=20
 is_present=1
 #variable
 p=$(( RANDOM % 2 ))
 
 #selection statement
+
 if [ $p -eq $is_present ]
 then
- echo "ABSENT"
+ emphr=8
 else
- echo "PRESENT"
+ emphr=0
 fi
+#calculation
 
+salary=$(( emphr * emp_wageperhr ))
+
+echo "$salary"
 
