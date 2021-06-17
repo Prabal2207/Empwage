b216ae13e370d4b6fee5b7ce36afd4127b8abb9c
Prabal2207 <prabal760@gmail.com>
2021-06-17 17:35:27 +0530

    part time full time cal is added

--- Employeecomputatio.sh
+++ Employeecomputatio.sh
@@ -3,14 +3,19 @@
 echo " welcome to empcomputation"
 emp_wageperhr=20
 is_present=1
+is_present_half=2
+
 #variable
-p=$(( RANDOM % 2 ))
+p=$(( RANDOM % 3 ))
 
 #selection statement
 
 if [ $p -eq $is_present ]
 then
  emphr=8
+elif [ $p -eq $is_present_half ]
+then
+ emphr=4
 else
  emphr=0
 fi
